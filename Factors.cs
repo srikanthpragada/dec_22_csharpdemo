0ad31c421fde27d4aab7ca71db8e0e164efba1ff
srikanthpragada <srikanthpragada@gmail.com>
2016-12-23 10:12:11 +0530

    23-DEC

---
+++ Factors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpdemo
+{
+    class Factors
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number :");
+            string input = Console.ReadLine();  // read a line from keyboard
+
+            int num = Int32.Parse(input);  // convert string to int
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                if (num % i == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharpdemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
